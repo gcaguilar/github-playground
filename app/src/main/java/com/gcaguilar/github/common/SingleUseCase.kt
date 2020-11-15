73cab0841b4db22eb3862ca02aa685294fe2981a
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2020-11-15 12:36:03 +0100

    Create SingleUseCase
    Create base class for uses case return single
    
    Affected: common/JobExecutor, common/ThreadExecutor, common/PostExecutor, common/SingleUseCase

---
+++ app/src/main/java/com/gcaguilar/github/common/SingleUseCase.kt
@@ -0,0 +1,24 @@
+package com.gcaguilar.github.common
+
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+abstract class SingleUseCase<T, in Params> constructor(
+    private val threadExecutor: ThreadExecutor,
+    private val postExecutionThread: PostExecutionThread
+) {
+
+    /**
+     * Builds a [Single] which will be used when the current [SingleUseCase] is executed.
+     */
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Single<T>
+
+    /**
+     * Executes the current use case.
+     */
+    fun execute(params: Params? = null): Single<T> {
+        return this.buildUseCaseObservable(params)
+            .subscribeOn(Schedulers.from(threadExecutor))
+            .observeOn(postExecutionThread.scheduler)
+    }
+}
